08a61ea8567adc6134fff9672a7e6ada2a373aea
sennychu <senny.share@gmail.com>
2013-09-26 20:12:56 +0800

    Create ncurses.rb

---
+++ Library/Formula/ncurses.rb
@@ -0,0 +1,50 @@
+require 'formula'
+
+class Ncurses < Formula
+  homepage 'http://www.gnu.org/s/ncurses/'
+  url 'http://ftpmirror.gnu.org/ncurses/ncurses-5.9.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz'
+  sha1 '3e042e5f2c7223bffdaac9646a533b8c758b65b5'
+
+ 
+  option :universal
+
+  # Fix building C++ bindings with clang
+  # def patches
+  #  { :p0 => "https://trac.macports.org/export/103963/trunk/dports/devel/ncurses/files/constructor_types.diff" }
+ #  end
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}",
+                          "--with-shared",
+                          "--enable-widec",
+                          "--with-manpage-format=normal",
+                          "--enable-symlinks"
+    system "make"
+    system "make install"
+    make_libncurses_symlinks
+  end
+
+  def make_libncurses_symlinks
+    major = version.to_s.split(".")[0]
+
+    cd lib do
+      %w{form menu ncurses panel}.each do |name|
+        ln_s "lib#{name}w.#{major}.dylib", "lib#{name}.dylib"
+        ln_s "lib#{name}w.#{major}.dylib", "lib#{name}.#{major}.dylib"
+        ln_s "lib#{name}w.a", "lib#{name}.a"
+        ln_s "lib#{name}w_g.a", "lib#{name}_g.a"
+      end
+
+      ln_s "libncurses++w.a", "libncurses++.a"
+    end
+
+    cd bin do
+      ln_s "ncursesw#{major}-config", "ncurses#{major}-config"
+    end
+  end
+end
